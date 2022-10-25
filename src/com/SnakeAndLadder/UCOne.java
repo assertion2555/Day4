ff58965a1a61a9a72885d1b0b3463f5f7b3472d8
saurabh <singhsaurabh7256@gmail.com>
2022-10-25 23:25:53 +0530

    Initial commit

---
+++ src/com/SnakeAndLadder/UCOne.java
@@ -0,0 +1,14 @@
+
+
+    public class UCOne {
+
+
+        public static void main(String[] args) {
+            int pos = 0;
+            System.out.println("Welcome to Snake And Ladder program");
+            System.out.println("Single player at start position "+ pos);
+
+        }
+
+    }
+
